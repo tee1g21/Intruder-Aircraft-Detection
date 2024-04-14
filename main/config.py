3467b0d2f73b6afcae95e869e2e33322be325e97
tee1g21 <92794120+tee1g21@users.noreply.github.com>
2024-04-14 02:03:16 +0100

    Implemented ClearML, created config file for constant project variables

---
+++ main/config.py
@@ -0,0 +1,5 @@
+BASE_DATASET = 'C:/github/Third-Year-Project/Intruder-Aircraft-Detection/datasets/AVOIDDS'
+PROJECT_NAME = 'Intruder-Aircraft-Detection'
+DATASET_DIR = 'C:/github/Third-Year-Project/Intruder-Aircraft-Detection/datasets'
+PROJECT_DIR = 'C:/github/Third-Year-Project/Intruder-Aircraft-Detection/logs'
+YOLO_CLASS_NAMES = ['aircraft']
