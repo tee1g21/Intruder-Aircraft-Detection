5b1e847faa2f639c72ca38469dc6ff243e4812b0
tee1g21 <92794120+tee1g21@users.noreply.github.com>
2024-04-14 22:12:44 +0100

    Updated config file

--- main/config.py
+++ main/config.py
@@ -1,5 +1,15 @@
+"""Python file containing constant project variables which will be used in testing but can only be changed in this file"""
+
+# Project variables
 BASE_DATASET = 'C:/github/Third-Year-Project/Intruder-Aircraft-Detection/datasets/AVOIDDS'
 PROJECT_NAME = 'Intruder-Aircraft-Detection'
-DATASET_DIR = 'C:/github/Third-Year-Project/Intruder-Aircraft-Detection/datasets'
-PROJECT_DIR = 'C:/github/Third-Year-Project/Intruder-Aircraft-Detection/logs'
+
+# YOLO specfic
+YOLO_DATASET_DIR = 'C:/github/Third-Year-Project/Intruder-Aircraft-Detection/datasets/Yolo'
+YOLO_PROJECT_DIR = 'C:/github/Third-Year-Project/Intruder-Aircraft-Detection/logs/Yolo'
 YOLO_CLASS_NAMES = ['aircraft']
+
+# Custom classifier specific
+CLF_DATASET_DIR = 'C:/github/Third-Year-Project/Intruder-Aircraft-Detection/datasets/Custom'
+CLF_PROJECT_DIR = 'C:/github/Third-Year-Project/Intruder-Aircraft-Detection/logs/Custom'
+CLF_CLASS_NAMES = ['Cessna Skyhawk','Boeing 737-800', 'King Air C90'] # TODO: give simpler class name (eg. cessna, boeing, king-air)
